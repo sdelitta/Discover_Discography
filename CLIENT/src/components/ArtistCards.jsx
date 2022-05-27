3b382f3edc3ddef4b518020e71c5f9e71b16fed2
Steven DeLitta <sdelitta@gmail.com>
2022-05-27 16:57:24 -0400

    Style modifications and reseeded DB

--- CLIENT/src/components/ArtistCards.jsx
+++ CLIENT/src/components/ArtistCards.jsx
@@ -4,11 +4,13 @@ import React from 'react'
 function ArtistCards(props) {
   return (
     <div className="artist-card" >
-      <li className="artist-item" id={props.artistName}>
-        <h3>Name: {props.artistName}</h3>
-        <h5>Members: {props.members}</h5>
-        <h5>Albums: {props.albums}</h5>
-      </li>
+      <div className="artist-content">
+        <li className="artist-item" id={props.artistName}>
+          <h3>Name: {props.artistName}</h3>
+          <h5>Members: {props.members}</h5>
+          <h5>Albums: {props.albums}</h5>
+        </li>
+      </div>
       <div className="img-wrapper">
         <img src={props.picture} alt={props.artistName}></img>
       </div>
