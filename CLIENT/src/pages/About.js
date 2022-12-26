989d78106cb73ef037a9783f47b933b18f6f6143
Steven DeLitta <sdelitta@gmail.com>
2022-12-26 13:46:16 -0500

    Cleaned styling on each page

--- CLIENT/src/pages/About.js
+++ CLIENT/src/pages/About.js
@@ -6,13 +6,13 @@ function About(props) {
   return (
     <div className='About'>
       <div className='artist-title'>
-      <h1>About This Site</h1>
+        <h1>About This Site</h1>
       </div>
-        <div className="about-section">
-          <h2>Discover Discography  is a community-maintained open source encyclopedia of music information. This means that anyone — including you — can help contribute to the project by adding information about your favorite artists and their collection of works.</h2>
-        </div>
+      <div className="about-section">
+        <h2>Discover Discography  is a community-maintained open source encyclopedia of music information. This means that anyone (including you!) can contribute to the project by adding information about your favorite artists and their collection of works.</h2>
       </div>
-    )
-  }
+    </div>
+  )
+}
   
   export default About
